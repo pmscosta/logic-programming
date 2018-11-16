1038c7892b5f7fe2314656fb04688bbaf66d8648
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-11-16 17:45:15 +0000

    Comentários

--- src/bot.pl
+++ src/bot.pl
@@ -4,6 +4,14 @@
 			========= Bot Play Levels =========
 			===================================
  **/
+
+/**
+ * botPlay(+Tab, +Player, -OutTab).
+ * Plays the random bot 
+ * @param Tab - Current Board
+ * @param Player - Current Player
+ * @param OutTab - Board after bot play
+ **/
 botPlay(Tab, Player, OutTab):-
 	valid_moves(Tab, Player, MovesList),
 	length(MovesList, L), 
@@ -12,6 +20,14 @@ botPlay(Tab, Player, OutTab):-
 	Move = [Row, Col, Dir], 
 	Piece is Player + 1, 
 	movePiece(Row, Col, Dir, Piece, Tab, OutTab).
+
+/**
+ * botPlayGreedy(+Tab, +Player, -OutTab).
+ * Plays the greedy bot 
+ * @param Tab - Current Board
+ * @param Player - Current Player
+ * @param OutTab - Board after bot play
+ **/
 
 botPlayGreedy(Tab, Player, OutTab):-
 	move_and_evaluate(Tab, Player, MovesList),
@@ -33,7 +49,14 @@ botPlayGreedy(Tab, Player, OutTab):-
 			===================================
  **/
 
-
+/*
+ * evaluateBoard(+Tab, +Player, -Value, +Length).
+ * Evaluates the board following an heuristic 
+ * @param Tab - Board to evaluate
+ * @param Player - Current Player
+ * @param Value - Board value following the heuristic
+ * @param Length - Length of the board
+*/
 evaluateBoard(Tab, Player, Value, Length):-
 	(
 		checkVictory(Tab, Player, Length),	Value = 100;
@@ -48,6 +71,16 @@ evaluateBoard(Tab, Player, Value, Length):-
 		sumlist(Score2, Value)
 	).
 
+/**
+ * evaluateMove(+Tab, +Player, +Move, -Value, +Length, -OutTab).
+ * Evaluates a move following an heuristic
+ * @param Tab - Current Board 
+ * @param Player - Current Player 
+ * @param Move - Move to evaluate 
+ * @param Value - Value from taking the move following the heuristic
+ * @param Length - Length of the board
+ * @param OutTab - Board after taking the move    
+ **/
 evaluateMove(Tab, Player, Move, Value, Length, OutTab):-
 	Move = [Row, Col, Move],
 	Piece is Player + 1,
@@ -55,6 +88,14 @@ evaluateMove(Tab, Player, Move, Value, Length, OutTab):-
 	evaluateBoard(OutTab, Player, Value, Length).
 
 
+/**
+ * checkTwoConnected(+Tab, +Player, +Length).
+ * Checks if two there is two pieces connected, horizontally,
+ * vertically or diagonally
+ * @param Tab - Current Tab
+ * @param Player - Current Player
+ * @param Length - Length of the board
+ **/
 checkTwoConnected(Tab, Player, Length):-
 	K is Length - 1,
 	(
@@ -63,19 +104,40 @@ checkTwoConnected(Tab, Player, Length):-
 		checkTwoBiggerDiagonals(Tab, Player, Length)
 	).
 
+/**
+ * isTwoConnected(+Player, +List).
+ * Verifies if there are two same pieces connected, by receiving a list of a line (diagonal, horizontal or vertical)
+ * @param Player - Current Player
+ * @param List - List of a diagonal or row or column from board
+ **/
 isTwoConnected(Player, List):-
 	Piece is Player + 1, 
 	sublist([Piece, Piece], List).
 
-checkTwoColumns(Tab, Player, N):-
-	between(0, N, N1), 
+/**
+ *checkTwoColumns(+Tab, +Player, +Length).
+ * Checks if there is two pieces connected together in two columns
+ * @param Tab - Current Board
+ * @param Player - Current Player
+ * @param Length - Board Length 
+ **/
+checkTwoColumns(Tab, Player, Length):-
+	between(0, Length, N1), 
 	getColumnN(Tab, N1, Col), 
 	isTwoConnected(Player, Col).
 
-checkTwoRows(Tab, Player, N):-
-	between(0, N, N1), 
+/**
+ *checkTwoRows(+Tab, +Player, +Length).
+ * Checks if there is two pieces connected together in two rows
+ * @param Tab - Current Board
+ * @param Player - Current Player
+ * @param Length - Board Length 
+ **/
+checkTwoRows(Tab, Player, Length):-
+	between(0, Length, N1), 
 	nth0(N1, Tab, Row), 
 	isTwoConnected(Player, Row).
+
 
 checkTwoBiggerDiagonals(Tab, Player, Length):-
 	K is Length - 2,
@@ -123,6 +185,7 @@ best(Tab, Player, State, [Move], Depth, BestTab, BestVal):-
 
 best(Tab, Player, State, [Move | NextMoves], Depth, BestTab, BestVal):-
 
+
 	Move = [Row, Col, Dir], 
 	Piece is Player + 1, 
 
@@ -134,7 +197,7 @@ best(Tab, Player, State, [Move | NextMoves], Depth, BestTab, BestVal):-
 
 	best(Tab, Player, State, NextMoves, Depth, Tab2, Val2), 
 
-	betterOf(OutTab, Val1, Tab2, Val2, BestTab, BestVal, State).
+	betterOf(OutTab, Val1, OutTab, Val2, BestTab, BestVal, State).
 
 
 betterOf(Tab1, Val1, _, Val2, Tab1, Val1, State) :-   
