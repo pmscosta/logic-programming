5ab9470dd23746130ec8a8a361047a6f983a2c11
Pedro Costa <up201605339@fe.up.pt>
2018-12-18 15:22:20 +0000

    i t  i s  w o r k i n g

--- proj2/src/powerstrike.pl
+++ proj2/src/powerstrike.pl
@@ -2,53 +2,59 @@
 :- use_module(library(random)).
 :- use_module(library(lists)).
 
+
+number_of_digits(Number, Digits):-
+        X = log(10, Number) + 1,
+        Digits is floor(X).
+
+createExpList(0, List, _, List).
+
+createExpList(Digits, List, Value, Output):-
+    NValue is Value * 10, 
+    append(List, [Value], NList),
+    NDigits is Digits - 1, 
+    createExpList(NDigits, NList, NValue, Output).
+
+createExpList(Digits, List):-
+    createExpList(Digits, [], 1,List).
+
 maxValue(First, Mult, Elems, Max):-
-    Exp = Elems - 1, 
-    Max is floor(First * ( Mult ** Exp)), 
-    printValues(First, Mult, Elems, Max).
+    Exp = Elems - 2, 
+    Max is floor(First * ( Mult ** Exp)).
 
 
-addConstrains([_, _], _).
+retractNumber(Number, MaxDigits, ExpList, Subtract, Exponent):-
+    Exponent in 1 .. MaxDigits, 
+    element(Exponent, ExpList, Exp),
+    Subtract #= ( Number // (10 * Exp) ) * Exp + (Number mod Exp).
 
-addConstrains([A, B | C], Mult):-
-    B #= A * Mult, 
-    addConstrains([B | C], Mult).
+addConstrains([_], _, _, _,List, List).
+
+addConstrains([A, B | C], Mult, MaxDigits, ExpList, ExpAcc, Exponents):-
+    retractNumber(A, MaxDigits, ExpList, SubtractNumber, Exp),
+    (B #= A * Mult) #\/ (A #>= 10 #/\ B #= SubtractNumber),
+    append(ExpAcc, [Exp], NExpAcc),
+    addConstrains([B | C], Mult, MaxDigits, ExpList, NExpAcc, Exponents).
 
 
-pstrike(First, Mult, Elems, Max):-
-    
+pstrike(First, Mult, Elems, Max, MaxDigits, ExpList):-
     length(Numbers, Elems),
-    domain(Numbers, 0, Max),
+    domain(Numbers, 1, Max),
     element(1, Numbers, First), 
     element(Elems, Numbers, First),
-    addConstrains(Numbers, Mult),
-    labeling([], Numbers), 
+    addConstrains(Numbers, Mult, MaxDigits, ExpList, [], Exponents),
+    append(Exponents, Numbers, Label),
+    statistics,
+    labeling([max_regret, bisect], Label), 
+    fd_statistics,
+    statistics, 
     write(Numbers).
-    
 
-    
-printValues(First, Mult, Elems, Max):-
-    write(First),nl, 
-    write(Mult), nl,
-    write(Elems), nl, 
-    write(Max),nl.
-
-
-play:-
-    random(2, 20, Mult),
-    random(0, 500, First),
-    random(3, 10, Elems), 
-    Circle is Elems + 1,
+start:-
+    Mult is 2, 
+    First is 6, 
+    Elems is 6,
     maxValue(First, Mult, Elems, Max),
-    pstrike(First, Mult, Circle, Max). 
-
-
-
-
-/*  ======== SPLIT NUMBER ===============  */
-numbers_atoms(Numbers, Atoms) :-
-    maplist(atom_number, Atoms, Numbers).
-
-digits_number(Digits, Number) :-
-    numbers_atoms(Digits, Atoms),
-    number_codes(Number, Atoms).
+    number_of_digits(Max, MaxDigits),
+    createExpList(MaxDigits, ExpList),
+    pstrike(First, Mult, Elems, Max, MaxDigits, ExpList). 
